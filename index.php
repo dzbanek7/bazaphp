6d296262210f036e412b4bf9be9af052786dfb13
dzbanek7 <adamgarnu@gmail.com>
2019-11-29 07:20:14 +0100

    dynamiczna klasa obsługi danych

--- index.php
+++ index.php
@@ -6,5 +6,19 @@ ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', 'errorlog.txt');
 
-echo "Witaj!";
+$kom = array();
+
+require_once('inc/db.php');
+require_once('inc/functions.php');
+
+$db = new Baza('baza/baza.db');
+
+if(isset($_GET['id']))
+	$id = $_GET['id'];
+else
+	$id = 1;
+
+$strona = array()
+
+include_once('inc/template.php');
 ?>
